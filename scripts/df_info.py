a4ebb6aae58755c217ba937634caea7b2b3907b2
Biruk Getaneh <bkgetmom@gmail.com>
2022-05-09 19:58:07 +0000

    Get column list

---
+++ scripts/df_info.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+class DataFrameInfo():
+    def __init__(self, df):
+        self.df = df.copy()
+
+
+    def get_columns_list(self):
+        '''
+        Return Column list of the Dataframe
+        '''
+        return self.df.columns.to_list()
