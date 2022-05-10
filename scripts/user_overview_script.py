2aeae765f31dd4dfae9a7904907dec14b11b9e42
Biruk Getaneh <bkgetmom@gmail.com>
2022-05-10 21:54:43 +0300

    Get the mobile handset group

--- scripts/user_overview_script.py
+++ scripts/user_overview_script.py
@@ -4,13 +4,24 @@ class UserOverviewScript():
     def __init__(self, df) -> None:
         self.df = df.copy()
 
+
     def get_top_handsets(self,  num):
         top_handset = self.df['Handset Type'].value_counts().head(num)
         return top_handset
+
 
     def get_top_manufacturers(self,  num):
         top_handset = self.df['Handset Manufacturer'].value_counts().head(num)
         return top_handset
 
     
-    
+    def get_handset_group(self):
+        top_3_manufacturers = self.get_top_manufacturers(3)
+
+        manufacturers = self.df.groupby("Handset Manufacturer")
+
+        for column in top_3_manufacturers.index:
+            result = manufacturers.get_group(column).groupby("Handset Type")['MSISDN/Number'].nunique().nlargest(5)
+            print(f"_______ { column } _______")
+            print(result)
+            print()
