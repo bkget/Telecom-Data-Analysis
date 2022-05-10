a9e2df2d6864aebab9f57334502311fd9c1b1d5b
Biruk Getaneh <bkgetmom@gmail.com>
2022-05-10 19:46:09 +0300

    Identify top 10 handset types used by customers

---
+++ scripts/user_overview_script.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+class UserOverviewScript():
+    def __init__(self, df) -> None:
+        self.df = df.copy()
+
+    def get_top_handsets(self,  num):
+        top_handset = self.df['Handset Type'].value_counts().head(num)
+        return top_handset
+
+    
+    
+    
