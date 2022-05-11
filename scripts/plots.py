4d69a1f744c537e792c1d74c2ccaa0931944ab8c
Biruk Getaneh <bkgetmom@gmail.com>
2022-05-11 14:36:05 +0300

    Plot a duration histogram

---
+++ scripts/plots.py
@@ -0,0 +1,10 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly.express as px
+import pandas as pd
+
+def hist(df:pd.DataFrame, column:str, color:str)->None:
+    plt.figure(figsize=(9, 7))
+    sns.displot(data=df, x=column, color=color, kde=True, height=7, aspect=2)
+    plt.title(f'Distribution of {column}', size=20, fontweight='bold')
+    plt.show()
