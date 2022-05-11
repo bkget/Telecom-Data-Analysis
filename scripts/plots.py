0f3a496c9cca20376d3a112751425ae6d5103436
Biruk Getaneh <bkgetmom@gmail.com>
2022-05-11 14:44:39 +0300

    Duation Box plot

--- scripts/plots.py
+++ scripts/plots.py
@@ -8,3 +8,11 @@ def hist(df:pd.DataFrame, column:str, color:str)->None:
     sns.displot(data=df, x=column, color=color, kde=True, height=7, aspect=2)
     plt.title(f'Distribution of {column}', size=20, fontweight='bold')
     plt.show()
+
+
+def box_plot(df: pd.DataFrame, x_col: str, title: str)->None:
+    plt.figure(figsize=(12, 7))
+    sns.boxplot(data=df, x=x_col)
+    plt.title(title, size=20)
+    plt.xticks(rotation=90, fontsize=14)
+    plt.show()
