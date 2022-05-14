79953d83394a159428de93a3f8b27f9265159656
Biruk Getaneh <bkgetmom@gmail.com>
2022-05-14 20:48:27 +0300

    Create an a method to display the data

--- dashboard/user_overview_page.py
+++ dashboard/user_overview_page.py
@@ -20,3 +20,19 @@ def loadPreprocessedData():
     df = pd.read_csv("../data/cleaned_Telecom_data.csv")
     return df
 
+
+def app():
+    st.title('TellCo Telecom Analytics Overview')
+
+    st.header('Table Description')
+    st.markdown(
+    '''
+        The Telecom Dataset has 150001 observations with 55 columns. 
+        Here is the description of each columns.
+    ''')
+    df = loadDescription()
+    st.write(df, width=1200)
+
+    st.header('Here is a sample Data from the Table')
+    df = loadOriginalData()
+    st.write(df.head(10))
