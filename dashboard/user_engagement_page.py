d7744e64f1222dce06cdd77e8b88f50b5b4ee53e
Biruk Getaneh <bkgetmom@gmail.com>
2022-05-14 20:50:31 +0300

    Create a user displaying function 2D + 3D

--- dashboard/user_engagement_page.py
+++ dashboard/user_engagement_page.py
@@ -39,3 +39,79 @@ def plotTop10(df):
         
         return plots.mult_hist([total_data_volume], 1, 1, "User Engagement Total Data Volume", ['Total Data Volume (kbps)'])
 
+
+def app():
+    st.title('User Engagement Analysis')
+    st.header("Top 10 customers per Engagement Metric")
+    user_engagement = getEngagemetData().copy()  
+    plotTop10(user_engagement)
+
+    st.header("Clustering customers based on their Engagement metric")
+    st.markdown(
+    '''
+        Here we will try to cluster customers based on their Engagement.
+        To start, choose the number of times to runs k-means.
+    ''')
+    num = st.selectbox('Select', range(0, 20))
+    select_num = 1
+    if(num != 0): 
+
+        st.markdown(
+        '''
+            Select the optimized values for k
+        ''')
+        select_num = st.selectbox('Select', range(1, num+1))
+
+    if(select_num != 1):
+
+        kmeans = KMeans(n_clusters=select_num, random_state=0).fit(user_engagement)
+        user_engagement.insert(0, 'Cluster', kmeans.labels_)
+
+        st.markdown(
+        '''
+            Number of elements in each cluster
+        ''')
+        st.write(user_engagement['Cluster'].value_counts())
+
+        show2D = False
+        if st.button('Show 2D visualization'):
+            if(show2D):
+                show2D = False
+            else:
+                show2D = True
+
+
+        if(show2D):
+            st.markdown(
+            '''
+                2D Visualization of Vluster
+            ''')
+
+            plots.scatter2d(user_engagement, x='Total_Data_Volume', y="Duration", c='Cluster', s='Session_Frequency') 
+        
+        show3D = False
+        if st.button('Show 3D Visualization'):
+            if(show3D == True):
+                show3D = False
+            else:
+                show3D = True
+        if(show3D):
+            st.markdown(
+            '''
+                3D Visualization of Cluster
+            ''')
+                       
+            plots.scatter3D(user_engagement, 'Total_Data_Volume', "Duration", 'Session_Frequency', 'Cluster', interactive=True) 
+        
+        st.warning(
+            'Remember cluster with the least engagement. we need that for satisfaction analysis')
+        st.markdown(
+        '''
+            Save the model for user satisfaction analysis
+        ''')
+        if st.button('Save Model'):
+            helper.save_csv(user_engagement,
+                            '../data/user_engagement.csv', index=True)
+
+            with open("../models/user_engagement.pkl", "wb") as f:
+                pickle.dump(kmeans, f)
