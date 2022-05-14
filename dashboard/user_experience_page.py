ccfc4167a617bcfc40c05addf1d52e9644a2132b
Biruk Getaneh <bkgetmom@gmail.com>
2022-05-14 20:52:05 +0300

    Get user experience data

---
+++ dashboard/user_experience_page.py
@@ -0,0 +1,34 @@
+import sys
+import os
+import sys
+import pandas as pd
+import streamlit as st
+import plotly.io as pio
+import plotly.express as px
+from sklearn.cluster import KMeans
+
+sys.path.append(os.path.abspath(os.path.join('../scripts'))) 
+import plots
+
+@st.cache
+def loadCleanData():
+    df = pd.read_csv("../data/user_experiance.csv")
+    return df
+
+@st.cache
+def getExperienceDataFrame():
+    df = loadCleanData().copy()
+    user_experience_df = df[[\
+        "MSISDN_Number", "Total_Avg_RTT", "Total_Avg_Bearer_TP", "Total_Avg_TCP"]].copy() 
+
+    return user_experience_df
+
+@st.cache
+def getExperienceData():
+    df = getExperienceDataFrame().copy()
+    user_experience = df.groupby('MSISDN_Number').agg({
+        'Total_Avg_RTT': 'sum',
+        'Total_Avg_Bearer_TP': 'sum',
+        'Total_Avg_TCP': 'sum'})
+    return user_experience
+
